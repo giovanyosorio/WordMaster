0bea6b1d02cdcc9b68ccdf2965bcbe2d12798e07
giovany osorio <giovany.torres.799@gmail.com>
2023-08-07 16:56:33 -0500

    Project JavaScript: Event Listener

---
+++ word-masters.js
@@ -0,0 +1,18 @@
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loading = document.querySelector(".info-bar");
+console.log(letters);
+async function init() {
+document.addEventListener("keydown", function (event) {
+    const action = event.key;
+
+    if (action === "Enter") {
+    commit();
+    } else if (action === "Backspace") {
+    removeLetter();
+    } else if (isLetter(action)) {
+    addLetter(action.toUpperCase());
+    }
+    });
+}
+
+init();
