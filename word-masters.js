7864d3e8b3beb79f340145028f14664ab0706831
giovany osorio <giovany.torres.799@gmail.com>
2023-08-08 16:47:58 -0500

    letters[ANSWER_LENGTH*currentWord+i].classList.add

--- word-masters.js
+++ word-masters.js
@@ -11,6 +11,8 @@ async function init() {
     const resObject=await response.json()
 
     const word=resObject.word.toUpperCase()
+    const wordParts=word.split("")
+    console.log(word);
     setLoading(false);
 
 
@@ -40,6 +42,30 @@ async function init() {
 
         // TODO do all the marking "correct" "close" or "wrong"
 
+        const guessParts=currentGues.split("")
+        const map=makeUp(guessParts)
+        console.log(map);
+        for (let i=0;i<ANSWER_LENGTH;i++){
+            if (guessParts[i]===wordParts[i]){
+                letters[ANSWER_LENGTH*currentWord+i].classList.add("correct")
+                map[guessParts[i]]--;
+            }
+           // else if (wordParts.includes(guessParts[i])){
+        }
+
+        for (let i = 0; i < ANSWER_LENGTH; i++) {
+            if (guessParts[i]===wordParts[i]) {
+               //do nothing
+            }else if (wordParts.includes(guessParts[i]) && map[guessParts[i]]>0) {
+                //  mark as  close
+                letters[ANSWER_LENGTH*currentWord+i].classList.add("close")
+                map[guessParts[i]]--;
+            }
+            else{
+                letters[ANSWER_LENGTH*currentWord+i].classList.add("wrong")
+            }
+            
+        }
         // TODO did they win or lose?
 
 
@@ -72,6 +98,20 @@ function isLetter(letter) {
 function setLoading(isLoading) {
     console.log("setLoading", isLoading);
     loading.classList.toggle("hidden", !isLoading);
+}
+
+function makeUp(array) {
+    const obj={};
+    for (let i=0;i<array.length;i++){
+        const letter=array[i];
+        if (obj[letter]){
+            obj[letter]++;
+        }else{
+            obj[letter]=1;
+        }
+    }
+    return obj;
+
 }
 
 init();
